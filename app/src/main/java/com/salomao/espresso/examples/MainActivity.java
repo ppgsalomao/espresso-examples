33faa8ced0f450574a47e9e47369dee962c21b92
pedro.salomao <ppgsalomao@gmail.com>
2016-01-25 15:31:31 -0200

    Adding code to be tested.

--- app/src/main/java/com/salomao/espresso/examples/MainActivity.java
+++ app/src/main/java/com/salomao/espresso/examples/MainActivity.java
@@ -1,7 +1,8 @@
 package com.salomao.espresso.examples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.findViewById(R.id.main_activity_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.findViewById(R.id.main_activity_text).setVisibility(View.GONE);
+            }
+        });
     }
 }
